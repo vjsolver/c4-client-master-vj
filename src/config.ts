710008851297cce4de65e8c6d96042fd9503d625
blockchaindev <blockchaindev@eisenhower.local>
2020-04-26 22:00:35 -0500

    initial commit from udacity project

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'avlz5drfhj'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'test-endpoint.auth0.com',
+  clientId: 'A72UMiQQJzmHQRPpe1dHOyq3EfPvo3Hy',
+  callbackUrl: 'http://localhost:3000/callback'
+}
